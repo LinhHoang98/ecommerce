7063d1cb8a3f19900124130f18e42cebb3d73d0a
Linh Hoang <linhhoang981221@gmail.com>
2024-09-20 17:01:32 +0700

    add cors

--- be-ecommerce/src/main/java/se/linhhn/ecommerce/BeEcommerceApplication.java
+++ be-ecommerce/src/main/java/se/linhhn/ecommerce/BeEcommerceApplication.java
@@ -2,6 +2,9 @@ package se.linhhn.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BeEcommerceApplication {
@@ -10,4 +13,15 @@ public class BeEcommerceApplication {
 		SpringApplication.run(BeEcommerceApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+			}
+		};
+	}
 }
